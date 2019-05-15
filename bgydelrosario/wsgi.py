71cbc6aadbcfb500e8ca1a15a03cb52c2523f48e
bitheaddhzdev <ajbitheaddhz@gmail.com>
2019-05-15 15:06:20 +0800

    inital repository

---
+++ bgydelrosario/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bgydelrosario project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bgydelrosario.settings')
+
+application = get_wsgi_application()

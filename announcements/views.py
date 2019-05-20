e621bf012ba6bddaecfe8db0d098db5afbbb1194
bitheaddhzdev <ajbitheaddhz@gmail.com>
2019-05-20 12:26:00 +0800

    End of chapter 43, made changes to index and about page

--- announcements/views.py
+++ announcements/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import Announcement
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 def index(request):
-    announcements = Announcement.objects.all()
-
+    announcements = Announcement.objects.order_by('-date').filter(is_published=True)
+    paginator = Paginator(announcements, 6)
+    page = request.GET.get('page')
+    page_announcements = paginator.get_page(page)
     context = {
-        'announcements' : announcements,    
+        'announcements' : page_announcements,    
     }
     return render(request, 'announcements/announcements.html', context)
 
-def announcement(request):
+def announcement(request, announcement_id):
     return render(request, 'announcements/announcement.html')
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from announcements.models import Announcement
+from officials.models import Official
 
 def index(request):
-    return render(request, 'pages/index.html')
-
+    announcements = Announcement.objects.order_by('-date').filter(is_published=True)[:3]
+    context = {
+        'announcements' : announcements,
+    }
+    return render(request, 'pages/index.html', context)
+    
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all officials
+    officials = Official.objects.order_by('-term_date')
+
+    # Get MVP
+    mvp_officials = Official.objects.all().filter(is_mvp=True)
+    context = {
+        'officials': officials,
+        'mvp_officials' : mvp_officials
+    }
+    return render(request, 'pages/about.html', context)

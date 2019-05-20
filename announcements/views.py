99bc77cfed46207c90814dbac1d77fcf23caa4bf
bitheaddhzdev <ajbitheaddhz@gmail.com>
2019-05-20 19:02:28 +0800

    end of chapter 44?

--- announcements/views.py
+++ announcements/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Announcement
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 def index(request):
     announcements = Announcement.objects.order_by('-date').filter(is_published=True)
     paginator = Paginator(announcements, 6)
@@ -12,7 +13,11 @@ def index(request):
     return render(request, 'announcements/announcements.html', context)
 
 def announcement(request, announcement_id):
-    return render(request, 'announcements/announcement.html')
+    announcement = get_object_or_404(Announcement, pk=announcement_id)
+    context = {
+        'announcement' : announcement
+    }
+    return render(request, 'announcements/announcement.html', context)
 
 def search(request):
     return render(request, 'announcements/search.html')
--- officials/models.py
+++ officials/models.py
@@ -8,6 +8,7 @@ class Official(models.Model):
     position = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    is_mvp = models.BooleanField(default=False)
     term_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.first_name + " " + self.last_name

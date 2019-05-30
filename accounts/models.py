3a8228c1a4395fdd58eea2b820d1970afdd2c7f7
bitheaddhzdev <ajbitheaddhz@gmail.com>
2019-05-30 11:36:42 +0800

    created UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthplace = models.CharField(max_length=200)
+    birthdate = models.DateField()
+    date_created = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.user.userprofile
+
